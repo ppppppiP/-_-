ea3837391bd3dd5d8d9823661afa253d8d65aaa1
artem <artem@DESKTOP-6MTUGES>
2023-06-08 22:36:38 +0300

    Добавьте файлы проекта.

---
+++ курсова_работа/PriorityQueue.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace курсова_работа
+{
+    internal class PriorityQueue<T>
+    {
+        private List<T> heap;
+        private List<int> priorities;
+
+        public int Count => heap.Count;
+
+        public PriorityQueue()
+        {
+            heap = new List<T>();
+            priorities = new List<int>();
+        }
+
+        public void Enqueue(T item, int priority)
+        {
+            heap.Add(item);
+            priorities.Add(priority);
+
+            int i = heap.Count - 1;
+            int parent = (i - 1) / 2;
+
+            // Поддержка свойства пирамиды (кучи)
+            while (i > 0 && priorities[parent] > priorities[i])
+            {
+                // Обмен значениями с родительским элементом
+                int tempPriority = priorities[i];
+                priorities[i] = priorities[parent];
+                priorities[parent] = tempPriority;
+
+                T tempItem = heap[i];
+                heap[i] = heap[parent];
+                heap[parent] = tempItem;
+
+                i = parent;
+                parent = (i - 1) / 2;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (heap.Count == 0)
+            {
+                throw new InvalidOperationException("Priority queue is empty.");
+            }
+
+            T item = heap[0];
+            heap[0] = heap[heap.Count - 1];
+            heap.RemoveAt(heap.Count - 1);
+
+            priorities[0] = priorities[priorities.Count - 1];
+            priorities.RemoveAt(priorities.Count - 1);
+
+            Heapify(0);
+
+            return item;
+        }
+
+        private void Heapify(int i)
+        {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int smallest = i;
+
+            if (left < heap.Count && priorities[left] < priorities[smallest])
+            {
+                smallest = left;
+            }
+
+            if (right < heap.Count && priorities[right] < priorities[smallest])
+            {
+                smallest = right;
+            }
+
+            if (smallest != i)
+            {
+                int tempPriority = priorities[i];
+                priorities[i] = priorities[smallest];
+                priorities[smallest] = tempPriority;
+
+                T tempItem = heap[i];
+                heap[i] = heap[smallest];
+                heap[smallest] = tempItem;
+
+                Heapify(smallest);
+            }
+        }
+    }
+}
---
+++ курсова_работа/Program.cs
@@ -0,0 +1,80 @@
+﻿using курсова_работа;
+
+PriorityQueue<int> priorityQueue = new PriorityQueue<int>();
+
+priorityQueue.Enqueue(5, 2);  // Добавление элемента со значением 5 и приоритетом 2
+priorityQueue.Enqueue(10, 1); // Добавление элемента со значением 10 и приоритетом 1
+priorityQueue.Enqueue(7, 3);  // Добавление элемента со значением 7 и приоритетом 3
+
+while (priorityQueue.Count > 0)
+{
+    int item = priorityQueue.Dequeue();
+    Console.WriteLine(item); // Вывод элементов очереди в порядке приоритета
+}
+
+// Пример использования пирамидальной сортировки
+int[] array = { 9, 4, 7, 1, 3, 6, 2, 8, 5 };
+
+HeapSort(array);
+
+foreach (int item in array)
+{
+    Console.Write(item + " "); // Вывод отсортированных элементов
+}
+    
+
+    // Пирамидальная сортировка
+    static void HeapSort(int[] array)
+{
+    int n = array.Length;
+
+    // Построение пирамиды (кучи)
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        Heapify(array, n, i);
+    }
+
+    // Извлечение элементов из пирамиды в отсортированный порядок
+    for (int i = n - 1; i > 0; i--)
+    {
+        // Обмен значениями корня (максимального элемента) с последним элементом
+        int temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
+
+        // Перестроение пирамиды для оставшейся части массива
+        Heapify(array, i, 0);
+    }
+}
+
+// Вспомогательный метод для перестроения пирамиды
+static void Heapify(int[] array, int n, int i)
+{
+    int largest = i; // Инициализация наибольшего элемента как корня
+    int left = 2 * i + 1; // Левый потомок
+    int right = 2 * i + 2; // Правый потомок
+
+    // Если левый потомок больше корня
+    if (left < n && array[left] > array[largest])
+    {
+        largest = left;
+    }
+
+    // Если правый потомок больше наибольшего элемента
+    if (right < n && array[right] > array[largest])
+    {
+        largest = right;
+    }
+
+    // Если наибольший элемент не корень
+    if (largest != i)
+    {
+        // Обмен значениями
+        int temp = array[i];
+        array[i] = array[largest];
+        array[largest] = temp;
+
+        // Рекурсивное перестроение пирамиды в поддереве
+        Heapify(array, n, largest);
+    }
+}
